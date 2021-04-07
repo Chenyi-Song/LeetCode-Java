b4d3bf197acf4621f76931b1fadfd3794d4bb96e
Chenyi.Song <chenyi.song@outlook.com>
2021-04-07 14:18:31 +0800

    Mod: to run No.001 in Main class

--- src/App.java
+++ src/App.java
@@ -1,6 +1,9 @@
 public class App {
+    
     public static void main(String[] args) throws Exception {
-        
+        // No. 001
+        No001 no001 = new No001();
+        no001.run();
     }
 
 }
